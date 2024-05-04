b0fb30cd8efaa3957ff8709ade756d99ed1f1fc3
Diogo Lima <75363687+diogooslima@users.noreply.github.com>
2024-05-04 20:06:03 +0200

    pagination updated: to make sure when the offset is changed in the URL, while clicking "previous" can go back to the very first result

--- src/services/process_pagination_links.js
+++ src/services/process_pagination_links.js
@@ -5,24 +5,40 @@ function processPaginationLinks(limit, offset, totalItems, tableNameParam) {
    const previousOffset = offset - limit;
    const lastOffset     = Math.floor((totalItems / limit)) * limit - 1;
 
+
+   
    // To build the first page link
    const firstPage = `http://localhost:3000/${tableNameParam}/?limit=${limit}&offset=${firstOffset}`;
 
+
+
    // To build the next page
    let nextPage = null;
+
    if (nextOffset < totalItems) {
       nextPage = `http://localhost:3000/${tableNameParam}/?limit=${limit}&offset=${nextOffset}`;
    };
 
+
+
    // To build the previous link
-   let prevPage = firstPage;
-   /* let prevPage = null; */
+   let prevPage = null;
+
+   // To make sure when the offset is changed in the URL, while clicking "previous" can go back to the very first result
+   if (offset > 0 && offset < limit) {
+      prevPage = firstPage;
+   };
+
    if (previousOffset >= 0) {
       prevPage = `http://localhost:3000/${tableNameParam}/?limit=${limit}&offset=${previousOffset}`;
    };
 
+
+
    // To build the last page link
    const lastPage = `http://localhost:3000/${tableNameParam}/?limit=${limit}&offset=${lastOffset}`;
+
+
 
    return {
       firstPage: firstPage,
